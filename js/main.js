15d9f55e5c4c715e97939c00f5443f5cc9c9400b
frsmtv <fsimitchiev@gmail.com>
2017-04-28 15:05:20 +0200

    zzz

--- js/main.js
+++ js/main.js
@@ -4,6 +4,40 @@ $(document).ready(function(){
   $('body').addClass('animated fadeIn');
   $('section').addClass('animated fadeIn');
   $('#teaser').addClass('animated fadeIn');
+  $('#social').addClass('animated fadeInLeft');
 
+  // MAIN MENU ACTIVE CLASS
+  $('#main-nav a').click(function(){
+    $(this).addClass('active');
+    $('#main-nav a').not($(this)).removeClass('active');
+    });
+
+    // NAV REDUCE ON SCROLL
+    var scroll_start = 0;
+    var startchange = $('body');
+    var offset = startchange.offset();
+    $(document).scroll(function() {
+        scroll_start = $(this).scrollTop();
+        if(scroll_start > offset.top) {
+            $('#main-nav a').css('font-size', '.6rem');
+            $('#logo').css('width', '100px');
+
+        } else {
+            $('#main-nav a').css('font-size', '');
+            $('#logo').css('width', '');
+        }
+    });
+
+  // SCROLLA
+  $('.animate').scrolla();
+
+  // STELLAR JS
+
+  // SCROLL TO
+  $("#hommage").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#hommage").offset().top
+    }, 2000);
+  });
 
 });
--- todo.md
+++ todo.md
@@ -1,12 +1,4 @@
 TODO :
 
-implement scrolla (http://scrollmagic.io/)
 implement modal for technique section
-implement active class for menu links :
-
-  jquery: $('#main-nav a').click(function(){
-    $(this).addClass('active');
-    $('#main-nav a').not(this).revmoveClass('active');
-    });
-
 implement parallax
